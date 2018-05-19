529031e198d396ca422a47831bc95a1c92e08cf3
LZ <1014456062@qq.com>
2018-05-19 17:16:29 +0800

    第二次上传

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
-import {robots} from "./robots.js";
 import SearchBox from "./SearchBox";
+import axios from "axios";
+import Scroll from "./Scroll";
 
 class App extends Component{
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         };
+    }
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then(users => {
+                this.setState({
+                    robots: users.data
+                })
+            })
     }
 
     onSearchChange = (event) => {
@@ -22,13 +32,29 @@ class App extends Component{
         const filteredRobots = this.state.robots.filter(robot => {
            return  robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         });
-        return (
-            <div className='tc'>
-                <h1 className='f2'>RobotFriends</h1>
-                <SearchBox search_change={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        // this.state.robots.length === 0 ?
+        //     return <h1>Loading</h1>
+        // :
+        // return (
+        //     <div className='tc'>
+        //         <h1 className='f2'>RobotFriends</h1>
+        //         <SearchBox search_change={this.onSearchChange}/>
+        //         <CardList robots={filteredRobots}/>
+        //     </div>
+        // );
+        if(this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        }else {
+            return (
+                <div className='tc'>
+                    <h1 className='f2'>RobotFriends</h1>
+                    <SearchBox search_change={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
